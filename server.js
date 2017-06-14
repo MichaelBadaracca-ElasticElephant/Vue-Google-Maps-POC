df5142ddc1ba1a4ee01ea20e0fd139efc83a56a0
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-14 12:17:16 -0600

    Added a simple map through the Google Maps api. API key is not stored on source control.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-var express = require('express')
+var express = require( 'express' )
+var secrets = require( "./secrets.js" );
 var app = express()
 
 app.use(express.static('./'))
 
-app.listen(3000)
+//The Google Maps script is loaded from the server to keep the API key out of source control and not easily revealed on the client side
+app.get( "/googleMapsApiScript", function ( req, res ) {
+    var googleMapsScriptUrlWithApiKey = secrets.googleMapsScriptUrlWithApiKey;
+    res.status( 200 );
+    res.send( googleMapsScriptUrlWithApiKey );
+});
+
+app.listen( 3000 );
+
