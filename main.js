ba1c9a18cd6de1a6a88d8072751d012ce5280ff8
Michael Badaracca - Elastic Elephant <michael.badaracca@elasticElephant.com>
2017-06-15 18:57:13 -0600

    Changed map refresh code to get rid of map flicker

--- main.js
+++ main.js
@@ -4,13 +4,14 @@ var mainAppVm = new Vue( {
         test: "TEST",
         startLat: -25.363,
         startLng: 131.044,
-        //directionsDisplay: null,
-        //directionsService: null
+        directionsDisplay: null,
+        directionsService: null,  
     },
 
 
     computed: {
         mapLocations: function () {
+            //this.initMap()
             return {
                 uluru: { lat: this.startLat, lng: this.startLng },
                 sydney: { lat: -33.868937, lng: 151.207788 },
@@ -25,6 +26,24 @@ var mainAppVm = new Vue( {
                 zoom: 4,
                 center: this.mapLocations.uluru
             });
+        },
+        uluruMarker: function () {
+            return new google.maps.Marker( {
+                position: this.mapLocations.mapLocationsuluru,
+                map: this.map
+            })
+        },
+        sydneyMarker: function () {
+            return new google.maps.Marker( {
+                position: this.mapLocations.sydney,
+                map: this.map
+            })
+        },
+        brisbaneMarker: function () {
+            return new google.maps.Marker( {
+                position: this.mapLocations.brisbane,
+                map: this.map
+            })
         }
 
     },
@@ -39,48 +58,22 @@ var mainAppVm = new Vue( {
             });
         },
         refreshMap: function () {
-            console.log( "Refreshing Map", this.mapLocations )
-            console.log("map",this.map)
             google.maps.event.trigger( this.map, 'resize' );
         },
 
         initMap: function () {
-            //this.mapLocations = {
-            //    uluru: { lat: -25.363, lng: 131.044 },
-            //    sydney: { lat: -33.868937, lng: 151.207788 },
-            //    brisbane: { lat: -27.465970, lng: 153.027510 },
-            //    wayPoints: [{ location: "Brisbane, Queensland", stopover: true },
-            //    { location: new google.maps.LatLng( - 24.990553, 151.954581 ), stopover: true }, { location: "Bundaberg, Queensland", stopover: true }]
-            //}
 
-            //var map = new google.maps.Map( document.getElementById( 'map' ), {
-            //    zoom: 4,
-            //    center: this.mapLocations.uluru
-            //});
+            this.directionsDisplay = new google.maps.DirectionsRenderer;
+            this.directionsService = new google.maps.DirectionsService;
+            this.directionsDisplay.setMap( this.map );
+            this.drawMap();
+        },
 
-            directionsDisplay = new google.maps.DirectionsRenderer;
-            directionsService = new google.maps.DirectionsService;
-            directionsDisplay.setMap( this.map );
-            this.calculateAndDisplayRoute( directionsService, directionsDisplay, this.mapLocations );
-
-
-            var uluruMarker = new google.maps.Marker( {
-                position: this.mapLocations.mapLocationsuluru,
-                map: this.map
-            });
-            var sydneyMarker = new google.maps.Marker( {
-                position: this.mapLocations.sydney,
-                map: this.map
-            });
-            var brisbaneMarker = new google.maps.Marker( {
-                position: this.mapLocations.brisbane,
-                map: this.map
-            });
+        drawMap: function () {
+            this.calculateAndDisplayRoute( this.directionsService, this.directionsDisplay, this.mapLocations );
         },
 
         calculateAndDisplayRoute: function ( directionsService, directionsDisplay, mapLocations ) {
-            console.log( "Map Locations", this.mapLocations );
-
             var selectedMode = "DRIVING"
             directionsService.route( {
                 origin: mapLocations.uluru,
